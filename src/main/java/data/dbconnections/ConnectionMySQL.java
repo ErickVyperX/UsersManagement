587d9836196e2f18d6c8306bc88c35d3363e1cda
Erick Villacorta <erickvillacorta98@gmail.com>
2022-09-30 19:35:51 -0500

    Pool Connection Version

--- src/main/java/data/dbconnections/ConnectionMySQL.java
+++ src/main/java/data/dbconnections/ConnectionMySQL.java
@@ -1,14 +1,23 @@
 package data.dbconnections;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class ConnectionMySQL {
     private static Connection connection;
     public static final String URL_JDBC = "jdbc:mysql://@localhost:3306/user_lab?user=root&password=Erick123!";
 
+    public static DataSource getDataSource() {
+        BasicDataSource bds = new BasicDataSource();
+        bds.setUrl(URL_JDBC);
+        bds.setInitialSize(5);
+        return bds;
+    }
+
     public static Connection getConnection() throws SQLException {
         if (connection == null) {
-            connection = DriverManager.getConnection(URL_JDBC);
+            connection = getDataSource().getConnection();
         }
         return connection;
     }
